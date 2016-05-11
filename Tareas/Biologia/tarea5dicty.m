1be2685439505645c016d33c2ebfb538f060bff5
Jose Alfonso Acosta Peñaloza <jjosealf94@gmail.com>
2016-05-11 11:47:15 -0500

    all
    desde sala

--- Tareas/Biologia/tarea5dicty.m
+++ Tareas/Biologia/tarea5dicty.m
@@ -8,7 +8,7 @@ Lx=length(x);	% numero de puntos
 dt=0.01;			% incremento temporal
 tmax=25;			% tiempo maximo
 jrec=50;			% intervalo de registro
-k=10;
+k=1.2;
 B=4;
 io=0.1;
 D=1e-07;
@@ -16,7 +16,7 @@ sigma=0.1;
 % initial conditions
 t=0;					% tiempo
 j=0;					% indice de registro
-
+cont=0;
 % inicializar c y i 
 c=exp(-1*x.^2/(2*sigma));
 i=io;
@@ -27,10 +27,13 @@ while t<tmax;
    % pintar cada jrec ciclos
    j=j+1;
    if(mod(j,jrec)==0)
-       figure();
-    	plot(x,c,'g',x,i,'r');
+       H=figure(); 
+       plot(x,c,'g',x,i,'r');
     	axis([-1 1 0 1])
     	drawnow;
+        saveas(H,['imagen' num2str(cont)],'png');
+        cont=cont+1;
+        cont
    end
    
    % segunda derivada
@@ -41,7 +44,7 @@ while t<tmax;
    ddc=(c1+c3-2*c2)/(dx*dx);			% segunda derivada de c
    
    dc=((c.^2/(B*i.^2+c.^2))-c+(D*ddc))*dt;	% dcdt
-   di=(k*(c-i))*dt;	% didt
+   di=(k.*(c-i))*dt;	% didt
    
    % actualizar variables
    c=c+dc;
